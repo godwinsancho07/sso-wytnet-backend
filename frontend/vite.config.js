9b70f48e47e4261323f2fdcdec9478ad125e6047
aysha-dheesan-banu <dheesanaysha@gmail.com>
2026-05-09 18:16:00 +0530

    udpatec ahgnes

---
+++ frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        auth: resolve(__dirname, 'auth.html'),
+        clients: resolve(__dirname, 'clients.html'),
+        users: resolve(__dirname, 'users.html'),
+      },
+    },
+  },
+});
